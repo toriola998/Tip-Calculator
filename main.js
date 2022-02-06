ef19bcc2552ba05293cd838736a6106d090ef2e4
Toriola Faidat <faaedah@gmail.com>
2022-02-06 18:34:58 +0100

    added calculate tip functionality

--- main.js
+++ main.js
@@ -1,20 +1,53 @@
 const billAmount = document.querySelector('.bill-amount');
 const noOfPerson = document.querySelector('.number-of-people')
 const customTip = document.querySelector('.custom');
-const tipButtons = document.querySelectorAll('.btn');
-const tipAmount = document.querySelector('.tip-amount')
+const tipButtons = [...document.querySelectorAll('.btn')]; //CONVERT NODELIST TO ARRAY
+const tipAmountPerPerson = document.querySelector('.tip-amount')
 const amountPerPerson = document.querySelector('.total-per-person')
 const resetBtn = document.querySelector('.reset')
 let moneyPerPerson;
 
-
-noOfPerson.addEventListener('input', () => { 
-    if (noOfPerson.value != "" || noOfPerson.value != 0)  {
-        moneyPerPerson =  parseInt(billAmount.value) / parseInt(noOfPerson.value);
-        amountPerPerson.textContent = `$${moneyPerPerson}`
-    }else {
-        amountPerPerson.textContent = `$0.00` 
-    } 
+//CALCULATE TIP WHEN A USER CLICKS ON A SELECT TIP BUTTON
+tipButtons.forEach( (btn) => {
+    btn.addEventListener('click', (e) => {
+        if(e.target.textContent == '5%'){
+           tipCalculation('5')
+        }
+        if(e.target.textContent == '10%'){
+            tipCalculation('10')
+         }
+         if(e.target.textContent == '15%'){
+            tipCalculation('15')
+         }
+         if(e.target.textContent == '25%'){
+            tipCalculation('25')
+         }
+         if(e.target.textContent == '50%'){
+            tipCalculation('50')
+         }
+    })
 })
 
+//CALCULATE TIPS & BILLS WHEN A USER INPUT IN THE CUSTOM BILL INPUT
+customTip.addEventListener('input', () => {
+    num = parseInt(customTip.value)
+    tipCalculation(num)
+})
 
+//REUSABLE FUNCTION FOR CALCULATING TIPS
+function tipCalculation (num){
+    let bill = parseInt(billAmount.value);
+    let totalTip = parseInt((bill * num) / 100)
+    let totalBillPlusTip = totalTip + bill;
+    console.log(totalBillPlusTip)
+    
+    noOfPerson.addEventListener('input', () => { 
+    if(noOfPerson.value != '' || noOfPerson.value != 0 ){
+        amountPerPerson.textContent = `$${totalBillPlusTip / parseInt(noOfPerson.value)}`
+    }else {
+        amountPerPerson.textContent = `$0.00` 
+    }
+
+    tipAmountPerPerson.textContent = `$${totalTip / parseInt(noOfPerson.value)}`
+    })
+}
