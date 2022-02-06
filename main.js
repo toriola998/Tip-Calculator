faf9bd89f38360e216de4280a480ecb1a1f8d636
Toriola Faidat <faaedah@gmail.com>
2022-02-06 16:19:05 +0100

    added calculate each person money functionality

--- main.js
+++ main.js
@@ -1,4 +1,20 @@
-const billAmount = document.querySelector('.bill');
+const billAmount = document.querySelector('.bill-amount');
+const noOfPerson = document.querySelector('.number-of-people')
 const customTip = document.querySelector('.custom');
 const tipButtons = document.querySelectorAll('.btn');
-const 
+const tipAmount = document.querySelector('.tip-amount')
+const amountPerPerson = document.querySelector('.total-per-person')
+const resetBtn = document.querySelector('.reset')
+let moneyPerPerson;
+
+
+noOfPerson.addEventListener('input', () => { 
+    if (noOfPerson.value != "" || noOfPerson.value != 0)  {
+        moneyPerPerson =  parseInt(billAmount.value) / parseInt(noOfPerson.value);
+        amountPerPerson.textContent = `$${moneyPerPerson}`
+    }else {
+        amountPerPerson.textContent = `$0.00` 
+    } 
+})
+
+
